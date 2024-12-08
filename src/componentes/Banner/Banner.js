2e33b977f9443bb7eb4da671b7ff0af56ff9c222
Loram117 <dougpsx360@gmail.com>
2024-12-08 17:55:09 -0300

    Alterações feitas conforme o curso

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX
+    return (
+        //<fragment> ou <> para colocar mais de um elemento html nesse componente
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+        //</fragment> ou </>
+    )
+}
